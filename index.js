415f26f82a8bdc3283bc513c76634c6dd0f23f3f
alexbgh1 <alex.garnica@alumnos.uach.cl>
2023-11-27 22:02:10 -0300

    Test: cors package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import comentariosRouter from "./routes/comentarios.route.js";
 import "./server/db/database.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 // Routes
 app.get("/", (req, res) => {
   res.send("API is running...");
